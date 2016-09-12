99518b2d4de70ccf15e464b44454bccb02a844eb
Lukaka4331 <105021044@live.asia.edu.tw>
2016-09-12 15:49:34 +0800

    hw00-1

---
+++ hw00-1/src/luuuuuu.java
@@ -0,0 +1,12 @@
+
+public class luuuuuu {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ uuu/src/uuu.java
@@ -0,0 +1,35 @@
+
+public class uuu {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("鐘岑哲資工系數位內容組");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
